ee57a9eb7003c0a25f27238b5481066875215e4f
gaurikuwar13 <gakulka25@gmail.com>
2019-11-14 16:26:43 -0500

    added UserController and demo for if statements

--- spring-boot-thymeleaf/src/main/java/com/example/springbootthymeleaf/controller/UserController.java
+++ spring-boot-thymeleaf/src/main/java/com/example/springbootthymeleaf/controller/UserController.java
@@ -9,7 +9,18 @@ public class UserController {
     @RequestMapping("demo")
     public String demo(Model model) {
         model.addAttribute("message","Hello Thymeleaf");
+        double grade = 59.99999;
+        model.addAttribute("grade",grade);
+        model.addAttribute("GPA",convertGPA(grade));
         //return to templates/demo.html page
         return "demo";
     }
+
+   public String convertGPA(double grade) {
+        if(grade>90) return "A";
+        else if (grade <=90 && grade >75) return "B";
+        else if (grade<=75 && grade >70) return "C";
+        else if (grade<=70 && grade >60) return "D";
+        else return "F";
+   }
 }
