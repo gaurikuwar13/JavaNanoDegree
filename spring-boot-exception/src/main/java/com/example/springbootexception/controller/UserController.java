dad79d82b22728c46e6e4ebf9f604ae0e1457242
gaurikuwar13 <gakulka25@gmail.com>
2019-11-16 07:48:40 -0500

    Added custom exception project

---
+++ spring-boot-exception/src/main/java/com/example/springbootexception/controller/UserController.java
@@ -0,0 +1,13 @@
+package com.example.springbootexception.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class UserController {
+    @RequestMapping("/add")
+    public String add() {
+        int num = 10/0;
+        return "add";
+    }
+}
